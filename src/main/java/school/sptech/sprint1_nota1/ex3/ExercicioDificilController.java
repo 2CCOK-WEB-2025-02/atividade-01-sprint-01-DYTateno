d1e4395ea551e90a762a5c3e3bcc60287ac494a2
DYTateno <diogo.tateno@sptech.school>
2025-08-20 23:37:13 -0300

    Exercicios dificil feito

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -7,8 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExercicioDificilController {
 
-    @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    @GetMapping(value = "/ex-03/{n}", produces = "application/json")
+    public String exercicioDificil(@PathVariable int n) {
+        if (n <= 0) {
+            return String.format("{\"enesimoTermo\": %d, \"soma\": %d}", 0, 0);
+        }
+
+        int a = 0;
+        int b = 1;
+        int enesimo = 0;
+        int soma = 0;
+
+        for (int i = 1; i <= n+1; i++) {
+            if (i == 1) {
+                enesimo = a;
+            } else if (i == 2) {
+                enesimo = b;
+            } else {
+                int temp = a + b;
+                a = b;
+                b = temp;
+                enesimo = temp;
+            }
+            soma += enesimo;
+        }
+
+        return String.format("{\"enesimoTermo\": %d, \"soma\": %d}", enesimo, soma);
     }
 }
